13e71ee730ab243bfd81febebd47ee50d357840d
louisrubin <miguel10.rbn@gmail.com>
2025-05-12 13:44:58 -0300

    min chang

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ export default function App() {
   return (
     <>
     <div className="text-3xl mt-50 ml-[30%] font-bold underline">
-      <span className="text-orange-500">*Rama MAIN*</span>
+      <span className="text-orange-500">*Rama DEV*</span>
 
       <h1 className="mt-15">
         Hello world!
