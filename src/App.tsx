97e4128e559274f27beab0f74acd7fe1167c2f4a
Martin khajhiantes Zamora <107328208+Khajhiantes@users.noreply.github.com>
2025-05-13 11:41:25 -0300

    a

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ export default function App() {
   return (
     <>
     <div className="text-3xl mt-50 ml-[30%] font-bold underline">
-      <span className="text-orange-500">*Rama DEV*</span>
+      <span className="text-orange-500">*RamA SALAmistica*</span>
 
       <h1 className="mt-15">
         Hello world!
