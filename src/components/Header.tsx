51690e012a7918a1f98d595772efd0eb39c1cf04
louisrubin <miguel10.rbn@gmail.com>
2025-05-13 23:08:10 -0300

    fix responsive --> header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,11 +6,13 @@ export default function Header() {
     <header className={`fixed top-0 left-0 w-full flex items-center justify-center bg-marron-bg transition-transform z-50
         py-4 px-2 shadow-inner border-marron-bg drop-shadow-[0_4px_6px_rgba(0,0,0,0.5)]`}>
         
-        <h1 className="h1-header ml-4 text-center text-3xl md:text-4xl font-[900] text-title-marron tracking-wide ">
-            UN HOGAR <span className="text-2xl md:text-3xl relative -top-0.5 inline-block">DE</span> BARRO
+        <h1 className="h1-header ml-4 text-center text-[7vw] md:text-4xl font-[900] text-title-marron tracking-wide ">
+            UN HOGAR <span className="text-[5.5vw] md:text-3xl relative -top-0.5 inline-block">DE</span> BARRO
         </h1>
-        <img src={hornero} width={'70vw'} className="drop-shadow-[0_4px_6px_rgba(255,255,255,0.8)]" />
 
+
+        <img src={hornero} width={'70vw'} className="drop-shadow-[0_4px_6px_rgba(255,255,255,0.8)]" />
+    
     </header>
   )
 }
