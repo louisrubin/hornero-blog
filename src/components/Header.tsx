097b74d3b122672cd4aef102205d2cb3f56c0880
louisrubin <miguel10.rbn@gmail.com>
2025-05-13 22:46:02 -0300

    text-shadow --> H1

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,7 +6,7 @@ export default function Header() {
     <header className={`fixed top-0 left-0 w-full flex items-center justify-center bg-marron-bg transition-transform z-50
         py-4 px-2 shadow-inner border-marron-bg drop-shadow-[0_4px_6px_rgba(0,0,0,0.5)]`}>
         
-        <h1 className="ml-4 text-center text-3xl md:text-4xl font-[900] text-title-marron tracking-wide ">
+        <h1 className="h1-header ml-4 text-center text-3xl md:text-4xl font-[900] text-title-marron tracking-wide ">
             UN HOGAR <span className="text-2xl md:text-3xl relative -top-0.5 inline-block">DE</span> BARRO
         </h1>
         <img src={hornero} width={'70vw'} className="drop-shadow-[0_4px_6px_rgba(255,255,255,0.8)]" />
