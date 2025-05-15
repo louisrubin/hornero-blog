01d23571c3fc907878037a0036440cc732a570a1
louisrubin <miguel10.rbn@gmail.com>
2025-05-15 19:50:04 -0300

    fix responsive

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -12,21 +12,20 @@ export default function Footer() {
             </h1>
 
             <div className='md:flex justify-center'>
-                <div className='absolute md:static bottom-4 w-1/2 flex flex-col items-center'>
-                    <img src={gobChaco} className=' w-43 h-30' 
+                <div className='absolute md:static bottom-4 w-1/2 md:w-1/5 flex flex-col items-center'>
+                    <img src={gobChaco} className=' w-43 h-30'
                     alt="Gobierno del Chaco" />
                     
-                    <img src={chacoCultura} className='w-28 h-10 ' 
+                    <img src={chacoCultura} className='w-28 h-10'
                     alt="Chaco Cultura" />
                     
-                    <img src={sociocultural} className=' w-40 h-38 ' 
+                    <img src={sociocultural} className='w-40 h-38'
                     alt="Sociocultural" />
                 </div>
 
                 <img src={horneroFooter} 
-                className='absolute md:static right-0 bottom-0 md:h-80 md:-ml-30 scale-x-[-1] drop-shadow-[0_4px_6px_rgba(0,0,0,0.6)]'
-                alt="Hornero | Ave Nacional" />
-                
+                className='absolute md:static right-0 bottom-0 md:h-80 lg:h-90 md:-ml-30 lg:ml-0 scale-x-[-1] drop-shadow-[0_4px_6px_rgba(0,0,0,0.6)]'
+                alt="Hornero | Ave Nacional" />                
             </div>
         </footer>
     )
